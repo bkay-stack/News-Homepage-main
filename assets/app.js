57d8688eb68a0332730e56afc424f2efb09b9c38
bkay-stack <kevinbest1900@gmail.com>
2024-02-05 19:13:33 +0100

    New codes

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,23 @@
-const menuBtn = document.querySelector('.menu img');
-const menu = document.querySelector('menu')
+const menuBtn = document.querySelector('.menu');
 const navBar = document.querySelector('.navbar');
-const xImg = document.querySelector('.x');
-
+const xImg = document.querySelector('.x img');
+const cover = document.querySelector('.cover');
 
 
 
 menuBtn.addEventListener('click', () => {  
-    navBar.classList.add('invisible')
-    // navBar.classList.add('x')
-    
+    navBar.classList.toggle('invisible')
+    cover.style.display = 'block';
+    menuBtn.style.display = 'none'
+    xImg.style.display = 'block'
    console.log('menu')
 })
 
 
 xImg.addEventListener('click', () => { 
-    // navBar.classList.toggle('invisible')
+    navBar.classList.toggle('invisible')
+    cover.style.display = 'none';
+    menuBtn.style.display = 'block'
+    xImg.style.display = 'none'
     console.log('x')
 })
